538cf3295f7cf9fbc272f56b314003f4cb699f3f
Hudayfa <hhw3103@hotmail.com>
2024-09-25 12:00:53 +0200

    Lavet den første Get:Posts

--- Program.cs
+++ Program.cs
@@ -1,42 +1,81 @@
-namespace RedditApi
+using Model;
+using System.Threading.Tasks;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Åben op for "CORS" i din API.
+// Læs om baggrunden her: https://docs.microsoft.com/en-us/aspnet/core/security/cors?view=aspnetcore-6.0
+
+var AllowCors = "_AllowCors";
+builder.Services.AddCors(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+    options.AddPolicy(name: AllowCors, builder => {
+        builder.AllowAnyOrigin()
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
 
-            // Add services to the container.
-            builder.Services.AddAuthorization();
+var app = builder.Build();
+app.UseCors(AllowCors);
 
 
-            var app = builder.Build();
+//List of all posts
+//List<Post> posts = new List<Post>();
+var user1 = new User("Hudayfa");
+//Post post = new Post(postText : "hh", user : user1, upvotes : 0, downvotes : 0);
+List<Post> posts = new List<Post>
+{
+    new Post("hh", user1),
+    new Post("hej", user1),
+    new Post("yo", user1),
 
-            // Configure the HTTP request pipeline. 1
+};
 
-            app.UseHttpsRedirection();
+//Get all Task
+app.MapGet("/api/posts", () => posts);
 
-            app.UseAuthorization();
 
-            var summaries = new[]
-            {
-                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-            };
+//app.MapPut("/api/tasks/{id:int}", (int id, Todo updatedTask) =>
+//{
+//    var task = ToDoList.FirstOrDefault(t => t.Id == id);
+//    if (task is null)
+//    {
+//        return Results.NotFound();
+//    }
 
-            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                    new WeatherForecast
-                    {
-                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                        TemperatureC = Random.Shared.Next(-20, 55),
-                        Summary = summaries[Random.Shared.Next(summaries.Length)]
-                    })
-                    .ToArray();
-                return forecast;
-            });
+//    task.Text = updatedTask.Text;
+//    task.Done = updatedTask.Done;
+//    return Results.NoContent();
+//});
 
-            app.Run();
-        }
-    }
-}
+
+
+////Put functionen til at ændre bool
+//app.MapPut("/api/tasks/{id}", (int id, Todo updatedTask) =>
+//{
+//    var task = ToDoList.FirstOrDefault(t => t.Id == id);
+//    if (task is null)
+//    {
+//        return Results.NotFound();
+//    }
+
+//    task.Text = updatedTask.Text;
+//    task.Done = updatedTask.Done;
+//    return Results.NoContent();
+
+//});
+
+
+
+app.MapGet("/", () => "Hello World!");
+
+
+app.MapGet("/api/hello/{name}", (string name) => new { Message = $"Hello {name}!" });
+
+
+
+app.Run();
+//record Task(int Id, string Text, bool Done);
+
+
---
+++ Service/Dataservice.cs
@@ -0,0 +1,14 @@
+﻿using Model;
+namespace Service
+{
+    //public class Dataservice
+    //{
+    //    private readonly PostContext _context;
+
+    //    public Dataservice (PostContext context)
+    //    {
+    //        _context = context;
+    //    }
+
+    //}
+}
